26ef362dd0df0c685289541841815dee77a25f4f
N7688 <lanxingrui@corp.netease.com>
2025-03-03 20:52:52 +0800

    1

---
+++ src/main/java/com/spring/lanxr/ai/godaitest/GodAiTestApplication.java
@@ -0,0 +1,14 @@
+package com.spring.lanxr.ai.godaitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodAiTestApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(GodAiTestApplication.class, args);
+    }
+
+}
